218cc0c689f3babf4473d2269740526dc8f84135
ajavieralejandro <amorosijavier@gmail.com>
2021-06-25 21:49:58 -0300

    connecting db

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const port = 3000;
+
+dotenv.config({
+  path: './config.env'
+});
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(con => {
+    console.log(con);
+    console.log('Database connected');
+  });
 
 app.get('/', (req, res) => {
   res.status(200).send('hello from server');
@@ -8,4 +30,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log('Im running');
+  console.log(process.env.PEP);
 });
